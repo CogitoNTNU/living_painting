1955493b11379085dd95f341650c126cc7fc5413
Didrik Sand <didrik.sand@gmail.com>
2022-10-04 19:16:11 +0200

    new picture

--- face_detection/distance.py
+++ face_detection/distance.py
@@ -82,7 +82,7 @@ def face_data(image):
 
 
 # reading reference_image from directory
-ref_image = cv2.imread("testImage2.jpg")
+ref_image = cv2.imread("testImage3.jpg")
 
 # find the face width(pixels) in the reference_image
 ref_image_face_width, x, y = face_data(ref_image)
