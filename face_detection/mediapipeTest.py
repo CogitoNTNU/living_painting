a78909e195ac3f67dd9b6432db99ccac2244523a
Didrik Sand <didrik.sand@gmail.com>
2022-10-25 18:31:23 +0200

    Cleaning

--- face_detection/mediapipeTest.py
+++ face_detection/mediapipeTest.py
@@ -2,6 +2,7 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import itertools
+import time
 
 
 GREEN = (0, 255, 0)
@@ -30,63 +31,11 @@ def get_facemesh_coords(face_landmarks, img):
     return xyz  # np.multiply(xyz, [w, h, w]).astype(int)
 
 
-def draw_mediapipe_facedetect():
-    # For webcam input:
-    cap = cv2.VideoCapture(0)
-    with mp_face_detection.FaceDetection(
-        model_selection=0, min_detection_confidence=1
-    ) as face_detection:
-        while cap.isOpened():
-            success, image = cap.read()
-            if not success:
-                print("Ignoring empty camera frame.")
-                # If loading a video, use 'break' instead of 'continue'.
-                continue
-
-            # To improve performance, optionally mark the image as not writeable to
-            # pass by reference.
-            image.flags.writeable = False
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            results = face_detection.process(image)
-
-            # Draw the face detection annotations on the image.
-            image.flags.writeable = True
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            if results.detections:
-                min_dist = -np.inf
-                for detection in results.detections:
-                    pos_right = mp_face_detection.get_key_point(detection, 4)
-                    pos_left = mp_face_detection.get_key_point(detection, 5)
-
-                    distance = pos_left.x - pos_right.x
-                    if distance > min_dist:
-                        min_dist = distance
-                        mp_drawing.draw_detection(image, detection)
-
-                # pos_right = mp_face_detection.get_key_point(detection, 4)
-                # pos_left = mp_face_detection.get_key_point(detection, 5)
-
-                # distance = pos_left.x - pos_right.x
-
-                cv2.putText(
-                    image,
-                    f"distance: {round(distance, 2)}",
-                    (400, 35),
-                    fonts,
-                    0.6,
-                    GREEN,
-                    2,
-                )
-            cv2.imshow("MediaPipe Face Detection", image)
-            if cv2.waitKey(1) == ord("q"):
-                break
-    cap.release()
-
-
 def draw_mediapipe_face_mesh():
     # For webcam input:
     cap = cv2.VideoCapture(0)
-    drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+    fps_list = []
+    mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
     with mp_face_mesh.FaceMesh(
         max_num_faces=1,
         refine_landmarks=True,
@@ -94,6 +43,7 @@ def draw_mediapipe_face_mesh():
         min_tracking_confidence=0.5,
     ) as face_mesh:
         while cap.isOpened():
+            t_start = time.time()
             success, image = cap.read()
             if not success:
                 print("Ignoring empty camera frame.")
@@ -135,16 +85,36 @@ def draw_mediapipe_face_mesh():
                     coords = get_facemesh_coords(face_landmarks, image)
                 cv2.putText(
                     image,
-                    f"angle: {round(coords[0], 2)}",
+                    f"x: {round(coords[0], 2)}",
                     (400, 35),
                     fonts,
                     0.6,
                     BLACK,
                     2,
                 )
+                cv2.putText(
+                    image,
+                    f"y: {round(coords[1], 2)}",
+                    (400, 80),
+                    fonts,
+                    0.6,
+                    BLACK,
+                    2,
+                )
+            fps_list.append(1 / (time.time() - t_start))
+            cv2.putText(
+                image,
+                f"fps: {round(np.mean(fps_list),2)}",
+                (100, 35),
+                fonts,
+                0.6,
+                BLACK,
+                2,
+            )
             # Flip the image horizontally for a selfie-view display.
             cv2.imshow("MediaPipe Face Mesh", image)
             if cv2.waitKey(1) == ord("q"):
+                print(len(fps_list))
                 break
     cap.release()
 
@@ -185,10 +155,12 @@ class face_mesh_obj:
 
 
 if __name__ == "__main__":
+    demo = True
     # for demostration purposes, get drawing of face-grid
-    draw_mediapipe_face_mesh()
-
-    # test with object, get coordniates out while doing video-capturing
-    # fmObj = face_mesh_obj()
-    # for coords in fmObj.detect_face():
-    #     print(coords[0])
+    if demo:
+        draw_mediapipe_face_mesh()
+    else:
+        # test with object, get coordniates out while doing video-capturing
+        fmObj = face_mesh_obj()
+        for coords in fmObj.detect_face():
+            print(coords[0])
