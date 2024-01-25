c395016a44f6bbd38228c0f78ac77881e8d07aa8
espen <espenbfo@stud.ntnu.no>
2024-01-25 14:59:35 +0100

    feat: add help to cmd arguments

--- main.py
+++ main.py
@@ -8,9 +8,21 @@ transition_speed = 0.05
 
 
 @click.command()
-@click.option("-a", "--animation-speed", default=0.1, help="")
-@click.option("-b", "--background-animation-speed", default=0.5, help="")
-@click.option("-t", "--transition-speed", default=0.05, help="")
+@click.option(
+    "-a", "--animation-speed", default=0.1, help="This controls the speed of the video"
+)
+@click.option(
+    "-b",
+    "--background-animation-speed",
+    default=0.5,
+    help="This controls the speed of the background",
+)
+@click.option(
+    "-t",
+    "--transition-speed",
+    default=0.05,
+    help="This controls the speed of the transition between styles",
+)
 def main(animation_speed, background_animation_speed, transition_speed):
     application(animation_speed, background_animation_speed, transition_speed)
 
